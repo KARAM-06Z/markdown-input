e2ab99b6499dd6d27e26981e5739e6ac5ccdeecb
KARAM-06Z <67804623+KARAM-06Z@users.noreply.github.com>
2025-03-14 15:35:03 +0300

    feat: Card

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Markdown Input</h1>
+  <div class="page">
+    <div class="d-flex justify-content-center align-items-center h-100">
+      <card class="w-75">
+        <span>Markdown Input</span>
+      </card>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent } from "vue";
+
+const Card = defineAsyncComponent(() => import("./components/card/index.vue"));
+</script>
