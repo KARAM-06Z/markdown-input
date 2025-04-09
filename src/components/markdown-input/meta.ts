ed534f61cf219ae3de428debfc26004c541120e7
KaramTSME <112618821+KaramTSME@users.noreply.github.com>
2025-04-09 09:00:15 +0300

    feat: markdown marker & parser WIP

--- src/components/markdown-input/meta.ts
+++ src/components/markdown-input/meta.ts
@@ -3,4 +3,60 @@ enum MarkdownInputActiveTab {
   Preview,
 }
 
-export { MarkdownInputActiveTab }
+enum MarkdownInputDropdownModeEnum {
+  Mention = 1,
+  Tag,
+}
+
+type TMarkdownMark = "link" | "custom-link" | "header" | "bold" | "italic" | "underline" | "ol" | "ul"
+type TSelection = { start: number; end: number }
+type TFindFirstWhitespaceIdx = { text: string; idx?: number; reverse?: TOptional<boolean>; byLine?: TOptional<boolean> }
+type TMarkdownInputDropdown = {
+  model: boolean
+  mode: TOptional<MarkdownInputDropdownModeEnum>
+  query: TOptional<string>
+}
+type TMarkdownMarkSelectionData = {
+  paragraphDivision: { preTagText: string; tagText: string; postTagText: string }
+  selectionMatch: RegExpMatchArray | null
+  selectionTrim: string
+  preWhiteSpace: string
+  postWhiteSpace: string
+}
+type TCustomLink = {
+  mode: MarkdownInputDropdownModeEnum
+  text: string
+  stringifiedData: string
+}
+type TCustomLinkArgs = { el: Element; data: any }
+type TModeAction = { mention?: (args: TCustomLinkArgs) => void; tag?: (args: TCustomLinkArgs) => void }
+type TMarkdownDropdownProps = Omit<TDropdownProps, "modelValue" | "resolveTrigger" | "teleport">
+type TMarkdownCustomKeyMapper = TKeyMapper & { stringify?: string[] }
+type TCustomMarkdownDropdownProps = TMarkdownDropdownProps & {
+  mention: {
+    promise: (filter: any) => Promise<any>
+    keyMapper?: TMarkdownCustomKeyMapper
+    initialOffset?: number
+    click?: (args: TCustomLinkArgs) => void
+  }
+  tag: {
+    promise: (filter: any) => Promise<any>
+    keyMapper?: TMarkdownCustomKeyMapper
+    initialOffset?: number
+    click?: (args: TCustomLinkArgs) => void
+  }
+}
+
+export { MarkdownInputDropdownModeEnum, MarkdownInputActiveTab }
+export type {
+  TMarkdownMark,
+  TSelection,
+  TFindFirstWhitespaceIdx,
+  TMarkdownInputDropdown,
+  TCustomLink,
+  TCustomMarkdownDropdownProps,
+  TMarkdownMarkSelectionData,
+  TMarkdownCustomKeyMapper,
+  TCustomLinkArgs,
+  TModeAction,
+}
