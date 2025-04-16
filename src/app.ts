e170df5da87b8f58687ea2293423790ba9f0d836
KaramTSME <112618821+KaramTSME@users.noreply.github.com>
2025-04-16 12:26:20 +0300

    feat: import prototype manipulation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import { createApp } from "vue"
 import "./app.scss"
+import "../prototypes"
 import App from "./App.vue"
+import { createApp } from "vue"
 import { fakeLink } from "./directives/fake-link"
 
 const app = createApp(App)
