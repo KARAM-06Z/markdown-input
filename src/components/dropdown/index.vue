13468177e04d2093f8cd2ea09126bf71d5d8eb84
KARAM-06Z <67804623+KARAM-06Z@users.noreply.github.com>
2025-04-20 21:50:51 +0300

    feat: Dropdown WIP

---
+++ src/components/dropdown/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dropdown-trigger" @click="trigger">
+    <slot name="trigger"></slot>
+  </div>
+  <slot name="content"></slot>
+</template>
+
+<script lang="ts" setup>
+import { toRef, watch } from "vue"
+import { getService } from "../../services"
+import type { ICoreService } from "../../core/interfaces/i-core-service"
+
+const props = defineProps({ modelValue: Boolean })
+const coreService = getService("core-service") as ICoreService,
+  modelValue = toRef(props, "modelValue")
+
+const trigger = () => {
+  console.log(coreService.getOverlay)
+}
+
+watch(() => modelValue.value, trigger)
+</script>
