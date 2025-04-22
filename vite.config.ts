6ef52fe41514c89e8d9dd17cdd6c5fb1b7ea8785
KARAM-06Z <67804623+KARAM-06Z@users.noreply.github.com>
2025-04-22 19:55:42 +0300

    0.0.21

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,12 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/markdown-input/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: [
-          "mixed-decls",
-          "color-functions",
-          "global-builtin",
-          "import",
-        ],
+        silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
       },
     },
   },
